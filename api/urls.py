db26d6f44aa8ca8dfd1baa2a696bfc0f396659c7
ilovedarknet <abboscik990@gmail.com>
2022-03-17 17:27:07 +0500

    added chat

--- api/urls.py
+++ api/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.test, name='tests')
+    path('', views.test, name='tests'),
+    path('email/',views.test_email, name='email'),
+    path('schedule_email/',views.schedule_mail, name='schedule_mail'),
 ]
--- api/views.py
+++ api/views.py
@@ -1,10 +1,21 @@
 from django.http import HttpResponse
-from django.shortcuts import render
 from .tasks import test_func
-# Create your views here.
+from email_send.task import send_mail_task
+from django_celery_beat.models import PeriodicTask, CrontabSchedule
+import json
+import random
 
 def test(request):
     test_func.delay()
     return HttpResponse("Done")
 
-    
+
+def test_email(request):
+    send_mail_task.delay()
+    return HttpResponse('Sent')
+
+
+def schedule_mail(request):
+    schedule, created = CrontabSchedule.objects.get_or_create(hour=1, minute=24)
+    task = PeriodicTask.objects.create(crontab=schedule, name="schedule_mail_task_" + "99", task='email_send.task.send_mail_task', args=json.dumps(([2, 3])))
+    return HttpResponse("Done")
